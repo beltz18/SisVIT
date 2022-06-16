c2e9b3909b871eda8eb552de8f6a18f30b042e56
beltz18 <beltz.18kyodai@gmail.com>
2022-06-16 15:04:31 -0400

    fixed style-troubles

--- infraccion.php
+++ infraccion.php
@@ -50,7 +50,7 @@
         <!-- <legend>Infracciones cometidas</legend><br>
         <?php// include './client/partials/option-infraction.php'; ?> -->
       </fieldset>
-
+    
       <button type="submit" id="btn_inf" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Registrar</button><br>
     </form>
   </div>
